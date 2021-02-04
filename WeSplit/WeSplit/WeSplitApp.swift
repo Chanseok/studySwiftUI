4dc56112ca2116ee934643698a3653aef94821e4
chanseok <hi007chans@gmail.com>
2021-02-04 22:17:10 +0900

    Create the project - WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 임찬석 on 2021/02/04.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

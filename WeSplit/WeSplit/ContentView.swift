107b8794023767880945d409228624373552fc6b
chanseok <hi007chans@gmail.com>
2021-02-05 17:55:26 +0900

    Almost complete UI layout by adding 1 more section for tipPercentage selection

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -25,6 +25,17 @@ struct ContentView: View {
                         }
                     }
                 }
+                
+                Section(header: Text("How much tip do you want to leave?")){
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }.textCase(.none)
+                
                 Section{
                     Text("$" + checkAmount)
                 }

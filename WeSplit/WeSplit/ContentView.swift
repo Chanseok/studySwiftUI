1bb7b8034729a7c4dc6056b9057ea2081f234bb1
chanseok <hi007chans@gmail.com>
2021-02-05 16:44:02 +0900

    Add input filed of TextField and Picker ( and NavigationView for Picker)

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage  = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100){
+                            Text("\($0) of people")
+                        }
+                    }
+                }
+                Section{
+                    Text("$" + checkAmount)
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+        
     }
 }
 
@@ -19,3 +40,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Memo: What I learned
+//
+// 1. Simulator에서 맥 키보드 대신에 Device의 키보드를 보이게하기 (⇧⌘K) I/O > Keyboard > Connect Hardware Keyboard를 Uncheck
+// 2. Picker를 NavigationView로 감싸니 그제서야 동작을 하는군요
